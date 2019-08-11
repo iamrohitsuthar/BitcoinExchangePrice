af8219d46083647e8c17fa5befa29caa74325738
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-11 18:39:51 +0530

    set adapter to the spinner for displaying the different currencies.

--- app/src/main/java/com/rohit/suthar/bitcoinexchangeprice/DashboardActivity.java
+++ app/src/main/java/com/rohit/suthar/bitcoinexchangeprice/DashboardActivity.java
@@ -1,10 +1,12 @@
 package com.rohit.suthar.bitcoinexchangeprice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -20,18 +22,23 @@ import org.json.JSONObject;
 public class DashboardActivity extends AppCompatActivity {
     private Context mContext;
     private TextView mTvPrice;
+    private AppCompatSpinner appCompatSpinner;
     private String API_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD";
+    private String currency[] = {"AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPD","XPF","XPT","YER","ZAR","ZMW","ZWL"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         initialize();
         fetchData();
+
+        appCompatSpinner.setAdapter(new ArrayAdapter(mContext,android.R.layout.simple_spinner_dropdown_item,currency));
     }
 
     private void initialize() {
         mContext = DashboardActivity.this;
         mTvPrice = findViewById(R.id.tv_price);
+        appCompatSpinner = findViewById(R.id.spinner_currency);
     }
 
     private void fetchData() {
