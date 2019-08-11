add8669444aedbd8f344cb5d481412f41c503d0c
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-11 19:33:56 +0530

    Add hint to the Spinner.

--- app/src/main/java/com/rohit/suthar/bitcoinexchangeprice/DashboardActivity.java
+++ app/src/main/java/com/rohit/suthar/bitcoinexchangeprice/DashboardActivity.java
@@ -1,12 +1,16 @@
 package com.rohit.suthar.bitcoinexchangeprice;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatSpinner;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
@@ -26,8 +30,8 @@ public class DashboardActivity extends AppCompatActivity {
     private TextView mTvPrice, mTvCurrencyType;
     private AppCompatSpinner appCompatSpinner;
     private String API_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
-    private String currency[] = {"AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPD","XPF","XPT","YER","ZAR","ZMW","ZWL"};
-    private String selected_item = null;
+    private String currency[] = {"Select currency", "AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPD","XPF","XPT","YER","ZAR","ZMW","ZWL"};
+    private String selected_item = "INR";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +39,46 @@ public class DashboardActivity extends AppCompatActivity {
         initialize();
         fetchData("INR");
 
-        appCompatSpinner.setAdapter(new ArrayAdapter(mContext,android.R.layout.simple_spinner_dropdown_item,currency));
+        ArrayAdapter arrayAdapter = new ArrayAdapter(mContext,android.R.layout.simple_spinner_dropdown_item,currency) {
+            @Override
+            public boolean isEnabled(int position) {
+                if(position == 0)
+                    return false;
+                else
+                    return true;
+            }
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                if(position == 0)
+                    ((TextView)view).setTextColor(Color.GRAY);
+                else
+                    ((TextView)view).setTextColor(Color.BLACK);
+                return view;
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                if(position == 0)
+                    ((TextView)view).setTextColor(Color.GRAY);
+                else
+                    ((TextView)view).setTextColor(Color.BLACK);
+                return view;
+            }
+        };
+
+        appCompatSpinner.setAdapter(arrayAdapter);
 
         appCompatSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selected_item = currency[position];
-                fetchData(appCompatSpinner.getSelectedItem().toString());
+                if(position != 0) {
+                    selected_item = currency[position];
+                    fetchData(currency[position]);
+                }
             }
 
             @Override
