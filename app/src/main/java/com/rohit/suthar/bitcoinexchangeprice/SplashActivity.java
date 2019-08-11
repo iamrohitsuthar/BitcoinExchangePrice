a8dca9f4ac299f7974db66721a85d7a025e80620
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-11 19:53:35 +0530

    add fullscreen flags in splash activity.

--- app/src/main/java/com/rohit/suthar/bitcoinexchangeprice/SplashActivity.java
+++ app/src/main/java/com/rohit/suthar/bitcoinexchangeprice/SplashActivity.java
@@ -5,19 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashActivity.this,DashboardActivity.class);
                 startActivity(intent);
+                finish();
             }
-        },1000);
+        },1200);
     }
 }
