b334c58cf7b5a5a973b130e3dbd4cf1f830bb1ce
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-11 19:16:03 +0530

    add dynamic load data by selected currency from spinner.

--- app/src/main/java/com/rohit/suthar/bitcoinexchangeprice/DashboardActivity.java
+++ app/src/main/java/com/rohit/suthar/bitcoinexchangeprice/DashboardActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.widget.AppCompatSpinner;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -21,9 +23,9 @@ import org.json.JSONObject;
 
 public class DashboardActivity extends AppCompatActivity {
     private Context mContext;
-    private TextView mTvPrice;
+    private TextView mTvPrice, mTvCurrencyType;
     private AppCompatSpinner appCompatSpinner;
-    private String API_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD";
+    private String API_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
     private String currency[] = {"AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPD","XPF","XPT","YER","ZAR","ZMW","ZWL"};
     private String selected_item = null;
     @Override
@@ -31,24 +33,39 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         initialize();
-        fetchData();
+        fetchData("INR");
 
         appCompatSpinner.setAdapter(new ArrayAdapter(mContext,android.R.layout.simple_spinner_dropdown_item,currency));
+
+        appCompatSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selected_item = currency[position];
+                fetchData(appCompatSpinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     private void initialize() {
         mContext = DashboardActivity.this;
         mTvPrice = findViewById(R.id.tv_price);
+        mTvCurrencyType = findViewById(R.id.tv_currency_type);
         appCompatSpinner = findViewById(R.id.spinner_currency);
     }
 
-    private void fetchData() {
+    private void fetchData(String currency) {
         //make volley request
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, API_URL, null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, API_URL+currency, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
                     updateView(response.getString("last"));
+                    Log.d("ROHIT",response.getString("last"));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -65,5 +82,6 @@ public class DashboardActivity extends AppCompatActivity {
 
     private void updateView(String res) {
         mTvPrice.setText(res);
+        mTvCurrencyType.setText(selected_item);
     }
 }
